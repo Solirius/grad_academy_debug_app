d489049efe1deb054fa3caaded642ed71a287572
Azarren <daniellekushnir22@gmail.com>
2024-07-24 13:01:36 +0100

    Language navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,17 @@
 import "bootstrap"
 import "@hotwired/turbo-rails"
+
+// app/javascript/application.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const languageSelect = document.getElementById('languageSelect');
+    const languageForm = document.getElementById('languageForm');
+  
+    if (languageSelect && languageForm) {
+      languageSelect.addEventListener('change', function() {
+        // Manually submit the form when the dropdown value changes
+        languageForm.submit();
+      });
+    }
+  });
+  
